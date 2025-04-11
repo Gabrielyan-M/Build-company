b845901f7d3a163212149347bcb51cc6388cf01e
Gabrielyan-M <gabrielyan.m77@gmail.com>
2025-04-11 13:20:38 +0400

    cursor.js

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,5 +1,6 @@
 const cursor = document.getElementById('cursor')
 const cursorPoint = document.getElementById('cursor-point')
+const cursorOff = document.querySelectorAll('.cursor-off')
 
 window.addEventListener('pointermove', (e) => {
 
@@ -19,44 +20,29 @@ document.addEventListener('mouseout', () => {
 document.querySelectorAll('.cursorEnter').forEach(cursorEnter => {
 
     cursorEnter.addEventListener('mouseenter', () => {
-        cursor.style.border = '3px solid rgb(129, 96, 52)'
-        cursorPoint.style.backgroundColor = 'rgb(129, 96, 52)'
-        
+        cursor.style.border = '2px solid rgba(255, 255, 255, 0.4)'
+        cursorPoint.style.backgroundColor = 'rgb(255, 255, 255)'
     })
 
     cursorEnter.addEventListener('mouseout', () => {
         cursor.style.border = '2px solid rgb(255, 255, 255)'
         cursorPoint.style.backgroundColor = 'rgb(255, 255, 255)'
-        
     })
+
 
 })
 
+cursorOff.forEach(cursorOff => {
+
+    cursorOff.addEventListener('mouseenter', () => {       
+        cursor.style.visibility = 'hidden'
+    })
+
+    cursorOff.addEventListener('mouseout', () => {
+        cursor.style.visibility = 'visible'
+    })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  delete
-document.addEventListener('keydown', (e) => {
-     if (e.key === 'F12') {
-        document.querySelector('html').classList.toggle('console-open')  
-     }
-        
 })
 
 
