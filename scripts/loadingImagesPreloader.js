e7b4c63347697b32c064fe53348ef177eac62a8d
Gabrielyan-M <gabrielyan.m77@gmail.com>
2025-04-14 14:28:21 +0400

    loadingImagesPreloader.js

--- scripts/loadingImagesPreloader.js
+++ scripts/loadingImagesPreloader.js
@@ -12,7 +12,7 @@ function loadingImagesToGallery () {
         
         for (let i = 0; i < imgPreloader.length; i++) {
             let ix = i + 1
-            imgPreloader[i].style.background = `url(../media/images/preload-gallery/image${ix}.png) center center / cover no-repeat`
+            imgPreloader[i].style.background = `url(./media/images/preload-gallery/image${ix}.png) center center / cover no-repeat`
 
             
         }           
