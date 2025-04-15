c729d12de9a5b31bbf4f35016fb9bc37d179e883
Gabrielyan-M <gabrielyan.m77@gmail.com>
2025-04-15 14:05:22 +0400

    burgerMenu.js

--- scripts/burgerMenu.js
+++ scripts/burgerMenu.js
@@ -1,6 +1,7 @@
 const burgerButton = document.querySelector('.burger-button')
 const burgerMenu = document.querySelector('.burger-menu')
 
+const anchorLinks = document.querySelectorAll('[data-js-anchor-link]')
 
 burgerButton.addEventListener('click', () => {
     
@@ -9,4 +10,15 @@ burgerButton.addEventListener('click', () => {
 
     document.querySelector('body').classList.toggle('scroll-off')
 
+})
+
+anchorLinks.forEach(anchorLink => {
+    anchorLink.addEventListener('click', () => {
+        
+        burgerButton.classList.remove('active-menu')
+        burgerMenu.classList.remove('active-menu')
+        
+        document.querySelector('body').classList.remove('scroll-off')
+
+    })
 })
